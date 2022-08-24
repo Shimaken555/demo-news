3781e616073a8ab284ccf59c0a28c80110902c26
shimaken <syou9ta14@gmail.com>
2022-08-24 23:55:59 +0900

    no message

--- src/component/sidebar/weather/weather.tsx
+++ src/component/sidebar/weather/weather.tsx
@@ -21,7 +21,7 @@ const Weather: React.FC = () => {
       const weatherKey = 'c19889a5d98ce4046ae35a0fd80dc0ff';
       const lat = 35.4122;
       const lon = 139.413;
-      const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=${exclude}&appid=${process.env.React_APP_WEATHER_API_KEY}`;
+      const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=${exclude}&appid=${weatherKey}`;
       const res = await axios.get(URL);
       setTemp(res.data.current.temp);
       setMain(res.data.current.weather[0].main);
