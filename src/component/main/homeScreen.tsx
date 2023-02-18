598fb9ee6836f6567ea2b2f708436391f0a49eeb
shimaken <syou9ta14@gmail.com>
2023-02-18 15:05:21 +0900

    サイト変更

--- src/component/main/homeScreen.tsx
+++ src/component/main/homeScreen.tsx
@@ -29,10 +29,11 @@ const HomeScreen: React.FC = () => {
       const newsKey = process.env.React_APP_NEWS_API_KEY;
       console.log(newsKey);
       // const URL = `https://newsdata.io/api/1/news?apikey=${newsKey}&country=jp&category=${category}&language=jp&`;
-      const URL = `https://newsapi.org/v2/top-headlines?country=jp&${category}=Business&apiKey=${newsKey}`;
+      // const URL = `https://newsapi.org/v2/top-headlines?country=jp&${category}=Business&apiKey=${newsKey}`;
+      const URL = `https://gnews.io/api/v4/top-headlines?category='business'&country=jp&apikey=${newsKey}`;
 
       const res = await axios.get(URL);
-      // setArticles(res.data.results);
+      console.log(res);
       setArticles(res.data.articles);
       setLoading(false);
     } catch (err: any) {
